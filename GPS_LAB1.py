da909e07a28bb12620d6bca8c6dda44fc3d17d84
Rajkaran Singh Grewal <rajkaran.singh.grewal@outlook.com>
2024-01-23 21:29:17 -0500

    Added MultiThreading Support

--- GPS_LAB1.py
+++ GPS_LAB1.py
@@ -3,36 +3,50 @@ from datetime import datetime
 from os.path import exists
 import pynmea2
 import time as TIME
+import threading
+import keyboard
+global_stop = 0
+def print_coord_string():
+    try:
+#    file_exists = os.path.exists("./Directions_from_72_Hoffman_Street_Kitchener_ON_to_Conestoga_College_Cambridge_-_Fountain_Street_campus_Fountain_Street_South_Cambridge_ON.nmea")
+        if global_stop != 1:
+            file = open("./Directions_from_72_Hoffman_Street_Kitchener_ON_to_Conestoga_College_Cambridge_-_Fountain_Street_campus_Fountain_Street_South_Cambridge_ON.nmea")
+            print("Opening File")
+            TIME.sleep(2)
+            count = 0
+            for line in file.readlines():
+                text = pynmea2.parse(line)
+                time = datetime.now().timestamp()
+                latitude = text.lat
+                longitude = text.lon
+                count = count + 1
+                Json = {"count": count, "app": "gps_test", "timestamp": int(time), "latitude": latitude,"longitude": longitude}
+                print(Json)
+                TIME.sleep(0.5)
+                if(global_stop == 1):
+                    exit()
 
-# now = datetime.now()
-# print("Now = ", now) # Current date and time
+        else:
+            print("The file does not exist on this path")
+    except Exception as err:
+        print(f"{type(err).__name__} was reached")
 
-# timestamp = now.timestamp()
-# msg = pynmea2.parse("$GPGGA,184353.07,1929.045,S,02410.506,E,1,04,2.6,100.00,M,-33.9,M,,0000*6D")
-# print(int(timestamp))
 
-file_exists = os.path.exists("F:\SEM 4\Pyhton data\LABS\LAB 1\mygeodata\Directions.nmea")
-if file_exists == True:
-  print(file_exists, ": The file exists")
-  file = open("F:\SEM 4\Pyhton data\LABS\LAB 1\mygeodata\Directions.nmea")
-  print("Opening File")
-
-  TIME.sleep(2)
-
-  count = 0
-  for line in file.readlines():
-      text = pynmea2.parse(line)
-      time = datetime.now().timestamp()
-      latitude = text.lat
-      longitude = text.lon
-      count = count + 1
-      if (count < 23):
-          Json = {"count": count, "app": "gps_test", "timestamp": int(time), "latitude": latitude,
-                  "longitude": longitude}
-          print(Json)
-          TIME.sleep(0.5)
-
-else:
-  print("The file does not exist on this path")
-
+def wait_for_close():
+    global global_stop
+    while(global_stop != 1):
+        if keyboard.is_pressed('space'):
+            global_stop = 1
+            print("Closing Program")
+            exit()
+        
+if __name__ == "__main__":
+    global_stop = 0
+    t1 = threading.Thread(target=print_coord_string)
+    t2 = threading.Thread(target=wait_for_close)
+    t1.start()
+    t2.start()
+    t1.join()
+  #  t2.join()
+    print_coord_string()
 
